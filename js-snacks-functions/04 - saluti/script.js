dc21ae8be3a02e21b2b9df61e48aaab635d01135
FrancescoTullio <francescotullio1992@gmail.com>
2024-11-06 17:39:04 +0100

    feat: quarto esercizio

--- js-snacks-functions/04 - saluti/script.js
+++ js-snacks-functions/04 - saluti/script.js
@@ -5,9 +5,9 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
-
+const saluta = (nome) => "ciao " + nome
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(saluta(name));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
