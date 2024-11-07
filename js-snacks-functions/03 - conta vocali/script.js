77508859f75dfed310d661d096a4f2050ae53528
FrancescoTullio <francescotullio1992@gmail.com>
2024-11-07 10:24:00 +0100

    feat:3

--- js-snacks-functions/03 - conta vocali/script.js
+++ js-snacks-functions/03 - conta vocali/script.js
@@ -5,15 +5,14 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 function vocali (parola){
-    const lowerword = parola.toLowerCase();
-    const parolaInArray = lowerword.split('');
-    const numeroVocali = 0;
-    for(let i = 0; i <= parolaInArray.lenght; i++){
-        if ((parolaInArray[i] === a) || (parolaInArray[i] === e) || (parolaInArray[i] === i) || (parolaInArray[i] === o) || (parolaInArray[i] === u)){
-            numeroVocali++
+    const lowerword = parola;
+    let numeroVocali = 0;
+    for(let i = 0; i < lowerword.length; i++){
+        if ((lowerword[i] === "a") || (lowerword[i] === "e") || (lowerword[i] === "i") || (lowerword[i] === "o") || (lowerword[i] === "u")){
+            numeroVocali++;
         }
-        return numeroVocali;
     }
+    return numeroVocali;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
